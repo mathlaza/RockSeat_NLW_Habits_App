67be5b3d954692ef5e73692fd802984b575e3631
Matheus Martinez <decretomusic@gmail.com>
2023-01-19 20:33:39 -0300

    Add  rota de criação de hábito

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,31 @@
 import { prisma } from "./lib/prisma";
 import { FastifyInstance } from "fastify"; // Para poder tipar o app
+import { z } from "zod"; // Valida os dados do request
+import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany()
-    return habits;
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(), // Title obrigatório, se não for, só add .nullable()
+      weekDays: z.array(z.number().min(0).max(6)) // Recebe array de números do 0 ao 6
+    })
+    const { title, weekDays } = createHabitBody.parse(request.body)
+
+    // startOf zera as horas, min e seg, e toDate retorna um date do javascript
+    const today = dayjs().startOf('day').toDate(); // Retorna a data atual com horários zerados
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map(weekDay => {
+            return {
+              week_day: weekDay,
+            }
+          })
+        }
+      }
+    })
   })
 }
