67747972a1c3205bb19d5f9723ad6a1ab988e94b
Matheus Martinez <decretomusic@gmail.com>
2023-01-21 08:17:15 -0300

    Mostra data e dias da semana reais em cada quadradinho

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@
 import { Header } from './components/Header';
 import { SummaryTable } from './components/SummaryTable';
 import './styles/global.css';
+import './lib/dayjs'; // Usar o dayjs em pt-br
 
 export function App() {
   return (
