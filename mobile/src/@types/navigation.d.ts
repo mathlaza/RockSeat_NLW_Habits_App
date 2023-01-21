a217eeb8ed5ded74aaa03cdbad99f472af66ce28
Matheus Martinez <decretomusic@gmail.com>
2023-01-20 23:33:33 -0300

    Definição da tipagem das rotas com o navigation.d.ts

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      new: undefined;
+      habit: {
+        date: string; // Levo essa data como parâmetro de uma tela pra outra
+      }
+    }
+  }
+}
