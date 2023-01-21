0bd3650566bb62c31b497f23b1630ca5a0fa3651
Matheus Martinez <decretomusic@gmail.com>
2023-01-21 08:28:58 -0300

    Limpa input e checkboxes após criar novo hábito

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
@@ -8,10 +9,21 @@ export function NewHabitForm() {
   const [title, setTitle] = useState('')
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
-  function createNewHabit(event: FormEvent) {
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault(); // Não fazer o redirecionamento de página
 
-    console.log(title, weekDays)
+    if (!title || weekDays.length === 0) { // Se user não preencheu título ou dia da semana
+      return // O formulário para aqui e não faz nada
+    }
+
+    await api.post('habits', {
+      title,
+      weekDays,
+    })
+
+    setTitle('');
+    setWeekDays([]);
+    alert('Hábito criado com sucesso!');
   }
 
   function handleToggleWeekDay(weekDay: number) {
@@ -37,6 +49,7 @@ export function NewHabitForm() {
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus // Quando o modal abrir, o input vai receber o foco do teclado
+        value={title}
         onChange={event => setTitle(event.target.value)}
       />
 
@@ -50,6 +63,7 @@ export function NewHabitForm() {
             <Checkbox.Root
               key={weekDay}
               className="flex items-center gap-3 group"
+              checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
