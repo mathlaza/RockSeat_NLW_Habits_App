ac81c49029fa470f877f6b4899bae55bca55cda6
Matheus Martinez <decretomusic@gmail.com>
2023-01-21 22:57:16 -0300

    Implementa marcação de hábitos no mobile

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,21 +1,70 @@
-import { ScrollView, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { BackButton } from "../components/BackButton";
 import dayjs from "dayjs";
 import { ProgressBar } from "../components/ProgressBar";
 import { Checkbox } from "../components/Checkbox";
+import { Loading } from "../components/Loading";
+import { api } from "../lib/axios";
 
 interface Params {
   date: string;
 }
 
+interface DayInfoProps {
+  completedHabits: string[];
+  possibleHabits: {
+    id: string;
+    title: string;
+  }[];
+}
+
 export function Habit() {
+  const [loading, setLoading] = useState(true);
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null);
+  const [completedHabits, setCompletedHabits] = useState<string[]>([]);
+
   const route = useRoute();
   const { date } = route.params as Params;
-
   const parsedDate = dayjs(date);
   const dayOfWeek = parsedDate.format('dddd');
   const dayAndMonth = parsedDate.format('DD/MM');
+
+  async function fetchHabits() {
+    try {
+      setLoading(true);
+
+      const response = await api.get('/day', { params: { date } });
+      setDayInfo(response.data);
+      setCompletedHabits(response.data.completedHabits);
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Ops', 'Não foi possível carregar as informações dos hábitos')
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleToggleHabit(habitId: string) {
+    if (completedHabits.includes(habitId)) {
+      setCompletedHabits(prevState => prevState.filter((habit) => habit !== habitId));
+    } else {
+      setCompletedHabits(prevState => [...prevState, habitId]);
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits();
+  }, []);
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
@@ -35,15 +84,17 @@ export function Habit() {
         <ProgressBar progress={30} />
 
         <View className="mt-6">
-          <Checkbox
-            title="Beber 2L de água"
-            checked={false}
-          />
-
-          <Checkbox
-            title="Caminhar"
-            checked={true}
-          />
+          {
+            dayInfo?.possibleHabits &&
+            dayInfo.possibleHabits.map((habit) => (
+              <Checkbox
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits.includes(habit.id)}
+                onPress={() => handleToggleHabit(habit.id)}
+              />
+            ))
+          }
         </View>
       </ScrollView>
     </View>
