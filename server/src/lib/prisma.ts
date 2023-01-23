09845e8d3dbf0e59ee1f9bc681d51ee46575248f
Matheus Martinez <decretomusic@gmail.com>
2023-01-23 19:13:09 -0300

    Tenta uma outra url

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  log: ['query'] // Faz o log das querys
-});
+// export const prisma = new PrismaClient({
+//   log: ['query'] // Faz o log das querys
+// });
+
+const prisma = new PrismaClient({ datasources: {  db: { url: `mysql://${process.env.DATABASE_URL}` } } });
