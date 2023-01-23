b74f7a22d9d878922258eed4af80a98ba434cfe1
Matheus Martinez <decretomusic@gmail.com>
2023-01-23 19:23:05 -0300

    Retorna a url anterior

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-// export const prisma = new PrismaClient({
-//   log: ['query'] // Faz o log das querys
-// });
-
-const prisma = new PrismaClient({ datasources: {  db: { url: `mysql://${process.env.DATABASE_URL}` } } });
+export const prisma = new PrismaClient({
+  log: ['query'] // Faz o log das querys
+});
