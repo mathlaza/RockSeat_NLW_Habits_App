b418bcdbe05d0b7555d748ffe404691ed6379f60
Matheus Martinez <decretomusic@gmail.com>
2023-01-23 16:34:16 -0300

    Verifica o que foi modificado

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -5,8 +5,8 @@ const prisma = new PrismaClient() // Faz a conexão com o banco de dados
 const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
 const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000')
 
-const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
-const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000')
+// const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
+// const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000')
 
 const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
 const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000')
@@ -36,20 +36,20 @@ async function run() {
       }
     }),
 
-    prisma.habit.create({
-      data: {
-        id: secondHabitId,
-        title: 'Exercitar',
-        created_at: secondHabitCreationDate,
-        weekDays: {
-          create: [
-            { week_day: 3 },
-            { week_day: 4 },
-            { week_day: 5 },
-          ]
-        }
-      }
-    }),
+    // prisma.habit.create({
+    //   data: {
+    //     id: secondHabitId,
+    //     title: 'Exercitar',
+    //     created_at: secondHabitCreationDate,
+    //     weekDays: {
+    //       create: [
+    //         { week_day: 3 },
+    //         { week_day: 4 },
+    //         { week_day: 5 },
+    //       ]
+    //     }
+    //   }
+    // }),
 
     prisma.habit.create({
       data: {
@@ -110,7 +110,7 @@ async function run() {
         dayHabits: {
           create: [
             { habit_id: firstHabitId },
-            { habit_id: secondHabitId },
+            // { habit_id: secondHabitId },
           ]
         }
       }
